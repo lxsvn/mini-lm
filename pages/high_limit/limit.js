b75d11e835629db1aae0288653787e3e80696f1f
马荣杰 <498292990@qq.com>
2018-05-23 18:06:10 +0800

    页面搭建

---
+++ pages/high_limit/limit.js
@@ -0,0 +1,20 @@
+// pages/high_limit/limit.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    proList: [
+      {}, {}, {}, {}, {}, {}, {}, {}, {}
+    ]
+  },
+
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
