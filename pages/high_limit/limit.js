2955a97839d22cfad4fb680f1a35d8b36f355c8a
马荣杰 <498292990@qq.com>
2018-05-24 13:04:38 +0800

    页面调整

--- pages/high_limit/limit.js
+++ pages/high_limit/limit.js
@@ -6,15 +6,33 @@ Page({
    */
   data: {
     proList: [
-      {}, {}, {}, {}, {}, {}, {}, {}, {}
+      { "s": 123 }, { "s": 456 }, { "s": 789}, {}, {}, {}, {}, {}, {}
     ]
   },
 
+  to_pro_detail: function (data) {
+    var index = data.currentTarget.dataset.index;
+    var proList=this.data.proList;
+    wx.setStorage({
+      key: 'proDetail',
+      data: proList[index],
+    })
+
+    wx.getStorage({
+      key: 'proDetail',
+      success: function(res) {
+        console.log(res.data.s)
+      },
+      fail: function(res) {},
+      complete: function(res) {},
+    })
+
+  },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-  
+
   }
 })
