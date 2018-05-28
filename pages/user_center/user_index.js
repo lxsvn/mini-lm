0661cf992b708726e97a91df20c7b0a4058b2e2e
Edward <lxzsrr@163.com>
2018-05-28 15:18:52 +0800

    1
    2

--- pages/user_center/user_index.js
+++ pages/user_center/user_index.js
@@ -1,66 +1,48 @@
-// pages/user_center/user_index.js
+//logs.js
+const util = require('../../utils/util.js')
+var config = require('../../config.js');
+
+var app = getApp()
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-  
+    msg:''
   },
+  onLoad: function () {
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-  
   },
+  getPhoneNumber: function (e) {
+   var that=this;
+    if (e.detail.iv != undefined) {
+      // 发送 res.code 到后台换取 openId, sessionKey, unionId
+      // //发起网络请求
+      wx.request({
+        url: `${config.apis.wxLoginSensitiveInfoUrl}`,
+        method: "POST",
+        data: {
+          encryptedData: e.detail.encryptedData,
+          iv: e.detail.iv,
+          openId: app.globalData.userInfo.openid,
+        },
+        success: function (data) {
+          that.setData({
+            msg: data.data.InnerData.Mobile
+          })
+          console.log(JSON.stringify(data.data))
+        }
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
-  },
+      })
+    }
+    else {
+      wx.showToast({
+        title: '拒绝',
+        icon: 'none',
+        duration: 500
+      })
+      return;
+    }
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
+    // console.log(e.detail.errMsg)
+    // console.log(e.detail.iv)
+    // console.log(e.detail.encryptedData)
   }
 })
